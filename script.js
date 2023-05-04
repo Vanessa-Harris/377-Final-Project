c03a20887de3e94d032dd61147ccb62ed67e399f
Vanessa <101517071+Vanessa-Harris@users.noreply.github.com>
2023-05-04 15:01:04 -0400

    start point before data change

--- script.js
+++ script.js
@@ -1,74 +1,74 @@
-function initBar(){
+function initBar() {
+  window.onload = function () {
+    var chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      exportEnabled: true,
+      theme: "light1", // "light1", "light2", "dark1", "dark2"
+      title: {
+        text: "Simple Column Chart with Index Labels",
+      },
+      axisY: {
+        includeZero: true,
+      },
+      data: [
+        {
+          type: "column", //change type to bar, line, area, pie, etc
+          //indexLabel: "{y}", //Shows y value on all Data Points
+          indexLabelFontColor: "#5A5757",
+          indexLabelFontSize: 16,
+          indexLabelPlacement: "outside",
+          dataPoints: [
+            { label: "105 Campus Connector", y: 71 },
+            { label: "116 Purple", y: 55 },
+            { label: "104 College Park Metro", y: 50 },
+            { label: "108 Adelphi", y: 65 },
+            { label: "115 Organge", y: 92, indexLabel: "\u2605 Highest" },
+            { label: "118 Gold", y: 21, indexLabel: "\u2691 Lowest" },
+          ],
+        },
+      ],
+    });
+    chart.render();
+  };
 
-    // set the dimensions and margins of the graph
-    const margin = {top: 30, right: 30, bottom: 70, left: 60},
-        width = 460 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
-    
-    // append the svg object to the body of the page
-    const svg = d3.select("#my_dataviz")
-      .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`);
-    
-    // Parse the Data
-
-    d3.json("https://api.umd.io/v1/bus/routes/116/schedules").then ( function(data) {
-    //( function(data) {
-      // sort data
-      data.sort(function(a,b,c,d,e) {
-        return a.Value - b.Value;
-      });
-    
-      // X axis
-      const x = d3.scaleBand()
-        .range([ 0, width ])
-        .domain(data.map(d => d.title))
-        .padding(0.2);
-      svg.append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-          .attr("transform", "translate(-10,0)rotate(-45)")
-          .style("text-anchor", "end");
-    
-      // Add Y axis
-      const y = d3.scaleLinear()
-        .domain([0, 13000])
-        .range([ height, 0]);
-      svg.append("g")
-        .call(d3.axisLeft(y));
-    
-      // Bars
-      svg.selectAll("mybar")
-        .data(data)
-        .enter()
-        .append("rect")
-          .attr("x", d => x(d.title))
-          .attr("y", d => y(d.Value))
-          .attr("width", x.bandwidth())
-          .attr("height", d => height - y(d.Value))
-          .attr("fill", "#69b3a2")
-    
-    })
-
-    return carto;
-
+  
 }
 
-async function mainEvent(){
-    const form = document.querySelector(".main_form");
-    const carto = initBar();
+async function mainEvent() {
+  const form = document.querySelector(".main_form");
+  const loadDataButton = document.querySelector("#data_load");
   
-    const results = await fetch(
-        "https://api.umd.io/v1/bus/routes/116/schedules"
-      );
+  const carto = initBar();
+
+  loadDataButton.addEventListener("click", async (submitEvent) => {
+    // async has to be declared on every function that needs to "await" something
+    console.log("Loading data"); // this is substituting for a "breakpoint"
+
+
+    const results = await fetch("https://api.umd.io/v1/bus/routes/108/schedules");
+
     // This changes the response from the GET into data we can use - an "object"
     const storedList = await results.json();
+    console.log(storedList);
+    //localStorage.setItem('storedData', JSON.stringify(storedList));
 
 
+   //  console.table(storedList); // this is called "dot notation"
+  
+
+  });
+
+  //Problem #1: I need to create a function that finds the frequency of a stop name
+  // in each bus's schedule api. ie. how many times does this bus stop at STAMP?
+  // first step = 
+  //Problem #2: I need to create an array that contains each bus name as x and 
+  //and the amount of time that bus goes to a selected stop . this array should
+  //change each time the user selects a new stop since it now must calculate a 
+  //different set of frequencies.
+  //lab 6 or 5, shows how to access data
+  //1st firgur why 12
+// how to extract data ie. stop_id
+// use a list of strongs where each string is a concatanation of bus number and stop id
 }
 
 /*
@@ -77,4 +77,3 @@ async function mainEvent(){
   In this case, we load some data when the form has submitted
 */
 document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
-
